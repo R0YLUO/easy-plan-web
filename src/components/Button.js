bdccf207f91e2ea9ad4340d43718fc06a1f41d62
YngTng <yangtang200448@gmail.com>
2023-09-09 20:36:38 +1000

    added fixed width to buttons

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = (props) => {
   return (
-    <button className="btn btn-neutral ml-10 mr-10">{props.name}</button>
+    <button className="btn btn-neutral ml-10 mr-10" style={{ width: '150px' }}>{props.name}</button>
   )
 }
 

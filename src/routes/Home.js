7897cfa415ec5453b1985c8ad764df959668904c
Roy Luo <r.luo2418@gmail.com>
2023-09-09 17:58:20 +1000

    Undo changes

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,18 +1,9 @@
 import React from 'react'
-import Button from '../components/Button'
 
 const Home = () => {
   return (
     <div>
       Home
-      <Button />
-      <Button></Button>
-      Yang
-      Eunice
-      Apple
-      Banana
-      Yang
-      Roy
     </div>
   )
 }

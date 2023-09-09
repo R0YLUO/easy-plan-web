71b6b11d01a3e6d245d441efc582646036a3512e
YngTng <yangtang200448@gmail.com>
2023-09-09 19:50:05 +1000

    added heading

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -3,11 +3,16 @@ import Button from '../components/Button'
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <Button name="Create group"/>
-      <Button name="Join group"/>
+    
+    <div className="min-h-screen flex flex-col justify-start items-center">
+      <h1 className="text-6xl font-bold mt-8">EasyPlan</h1>
+      <div className="flex ">
+        <Button name="Create group" />
+        <Button name="Join group" />
+      </div>
     </div>
-  )
+  );
 }
 
 export default Home
+

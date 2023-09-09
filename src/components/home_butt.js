af3c91515ccacbc94c2a72e37a7569f9270ebcff
YngTng <yangtang200448@gmail.com>
2023-09-09 22:17:46 +1000

    removed a line of code

--- src/components/home_butt.js
+++ src/components/home_butt.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Button from '../components/Button';
 import { ReactComponent as CustomSVG } from '../components/button_icon/home.svg';
 
 const HomeButton = () => {

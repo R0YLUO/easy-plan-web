d9d5a6f2d002c029967378a226904da44632c4fa
YngTng <yangtang200448@gmail.com>
2023-09-10 11:21:06 +1000

    formated button position

--- src/routes/Group.js
+++ src/routes/Group.js
@@ -9,29 +9,37 @@ const Group = () => {
   return (
     
     <div className="text-center">
-      <HomeButton className ="float-left"/>
+      <div className="flex justify-between items-start">
+        <HomeButton />
+      </div>
 
-      <h1 className="text-4xl font-bold mt-10">Group X</h1>
+      <div className="flex flex-col items-center">
+        <h1 className="text-4xl font-bold mt-10">Group X</h1>
 
-      <div className="text-2xl mt-10">Invite your friends:</div>
-      
-      <div className="flex justify-center mt-5">
-      <CopyTextbox text="https://EasyPlan.com/invite?id=xdfx123"/>
-      </div> 
+        <div className="text-2xl mt-10">Invite your friends:</div>
 
-        <div className="w-96 h-96 bg-blue-600 m-20 p-10 text-white text-center flex rounded-lg">
-          <h1 className="font-bold p-4">Group Members</h1>
-          <br />
+        <div className="flex justify-center mt-5">
+          <CopyTextbox text="https://EasyPlan.com/invite?id=xdfx123" />
+        </div>
+        <div className="flex justify-center">
+          <div className="w-96 h-96 bg-blue-600 m-20 p-10 text-white flex rounded-lg">
+            <h1 className="font-bold p-4">Group Members</h1>
+            <br />
             <ul className="mt-5 p-4 text-left">
               <li className="mb-2">Member 1</li>
               <li className="mb-2">Member 2</li>
               <li className="mb-2">Member 3</li>
             </ul>
-        <div className="flex justify-end">
-          <Button name="Update Preferences" />
-          <Button name="Generate Restaurants" />
+          </div>
+
+          <div className="flex justify-center items-center flex-col">
+            <Button name="Update Preferences" />
+            <br />
+            <Button name="Generate Restaurants" />
+          </div>
+
         </div>
-      </div>      
+      </div>
     </div>
 
   )

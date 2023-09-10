4f69a1eb579b5ac6f8c1cea66802d143b0fdf4e3
YngTng <yangtang200448@gmail.com>
2023-09-10 12:07:05 +1000

    replaced filler text with desc

--- src/routes/Group.js
+++ src/routes/Group.js
@@ -22,7 +22,6 @@ const Group = () => {
           <CopyTextbox text="https://EasyPlan.com/invite?id=xdfx123" />
         </div>
 
-
         <div className="flex justify-between">
           <div className="w-96 h-96 bg-neutral/75 m-20 p-5 text-white flex flex-col justify-start items-center rounded-lg">
             <h1 className="text-xl font-bold p-2">Group Members</h1>
@@ -32,7 +31,6 @@ const Group = () => {
               <li className="mb-2">Member 3</li>
             </ul>
           </div>
-
 
           <div className="flex flex-col justify-center">
             <Button name="Update Preferences" />

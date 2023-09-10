e69847ad9fe9b570802677eefc96ac68bdf16815
eeunicey <yaptingern@gmail.com>
2023-09-10 13:27:11 +1000

    recs page fixed spacing and refined boxes

--- src/routes/Recommendations.js
+++ src/routes/Recommendations.js
@@ -3,34 +3,53 @@ import React from 'react'
 const Recommendations = () => {
   return (
     <div>
-      <div className="flex justify-center mt-20">
-          <div className="w-96 h-500px bg-neutral/75 mt-20 p-5 text-white flex rounded-lg flex-col space-y-5 jusitfy-center">
-            <div className="w-80 h-fit bg-neutral-400/25 p-10 text-white flex rounded-lg">
-                <div className="text-center">
-
+      <div className="flex justify-center m-auto">
+        <div>
+          
+        </div>
+          <div className="w-96 h-fit bg-neutral/75 mx-10 my-20 p-5 text-white flex rounded-lg flex-col space-y-5 jusitfy-center">
+            <div className="w-full h-fit bg-neutral-400/25 p-5 text-white flex rounded-lg">
+                <div className="text-left">
+                  Restaurant:
+                  <br/>
+                  Cuisine:
+                  <br/>
+                  Distance:
                 </div>
             </div>
-            <div className="w-80 h-fit bg-neutral-400/25 p-10 text-white flex rounded-lg">
-                <div className="text-center">
-
+            <div className="w-full h-fit bg-neutral-400/25 p-5 text-white flex rounded-lg">
+                <div className="text-left">
+                  Restaurant:
+                  <br/>
+                  Cuisine:
+                  <br/>
+                  Distance:
                 </div>
             </div>
-            <div className="w-80 h-fit bg-neutral-400/25 p-10 text-white flex rounded-lg">
-                <div className="text-center">
-
+            <div className="w-full h-fit bg-neutral-400/25 p-5 text-white flex rounded-lg">
+                <div className="text-left">
+                  Restaurant:
+                  <br/>
+                  Cuisine:
+                  <br/>
+                  Distance:
                 </div>
             </div>
-            <div className="w-80 h-fit bg-neutral-400/25 p-10 text-white flex rounded-lg">
-                <div className="text-center">
-
+            <div className="w-full h-fit bg-neutral-400/25 p-5 text-white flex rounded-lg">
+                <div className="text-left">
+                  Restaurant:
+                  <br/>
+                  Cuisine:
+                  <br/>
+                  Distance:
                 </div>
             </div>
           </div>
           
           
 
-          <div className="w-96 h-500px bg-neutral/75 m-20 p-10 text-white flex rounded-lg">
-
+          <div className="w-96 h-500px bg-neutral/75 mx-10 my-20 p-5 text-white flex rounded-lg">
+          <div className="w-full h-2/3 bg-neutral-400/25 p-5 text-white flex rounded-lg"></div>
           </div>
       </div>      
     </div>
